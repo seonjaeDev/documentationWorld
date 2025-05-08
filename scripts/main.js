608aa4bcfe215d5c73b1417012d59f4fe5436fec
seonjaeDev <pfsn.valdonjefferson@outlook.com>
2025-05-08 19:14:17 +0800

    wip: replace nav a with button\n done: nav sideBar functionality working

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+function showSideBar() {
+  document.getElementById("sideBar").style.left = "0px";
+  document.getElementById("blankSpace").style.display = "block";
+}
+
+function hideSideBar() {
+  document.getElementById("sideBar").style.left = "-250px";
+  document.getElementById("blankSpace").style.display = "none";
+}
